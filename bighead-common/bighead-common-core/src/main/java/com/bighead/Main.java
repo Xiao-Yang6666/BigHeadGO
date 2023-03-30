c12e2408a4a41a1ea2b88bc876b036090f538599
xiaoyang <1007006957@qq.com>
2023-03-30 17:33:32 +0800

    添加公共模块，公共redis模块。

---
+++ bighead-common/bighead-common-core/src/main/java/com/bighead/Main.java
@@ -0,0 +1,11 @@
+package com.bighead;
+
+/**
+ * @author XiaoYang
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
